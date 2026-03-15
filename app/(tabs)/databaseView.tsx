7267d8f94fcc42e1d110a1154c9b391f64fa9f98
Eli <eli.comp07@protonmail.com>
2026-03-15 16:38:10 +0000

    working basic database

--- app/(tabs)/databaseView.tsx
+++ app/(tabs)/databaseView.tsx
@@ -1,6 +1,7 @@
 import * as SQLite from 'expo-sqlite';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { useState } from 'react';
 
 
 interface device{
@@ -11,43 +12,49 @@ interface device{
 
 
 async function createTablesIfNotExist(db:SQLite.SQLiteDatabase){
-    let query = "CREATE TABLE IF NOT EXISTS devices(macaddress TEXT PRIMARY KEY, deviceName TEXT NOT NULL;"
-        + "INSERT INTO devices (macaddress, deviceName) VALUES ('AA:AA:AA', 'headphones');"
-
+    let query = "CREATE TABLE IF NOT EXISTS devices (macaddress TEXT PRIMARY KEY, deviceName TEXT NOT NULL);"
+    console.log(query)
     await db.execAsync(query)
 }
 async function addDeviceToDatabase(db:SQLite.SQLiteDatabase,mac:string, name:string){
     //sanitise name and make sure mac is valid 
-    await db.runAsync("INSERT INTO devices (macaddress, deviceName) VALUES (?, ?)",mac,name)
+    await db.runAsync("INSERT INTO devices (macaddress, deviceName) VALUES (?, ?);",mac,name)
 
 }
 
 async function getDatabase(name:string){
 
     const db = await SQLite.openDatabaseAsync(name)
+    console.log("creating tables")
     await createTablesIfNotExist(db)
+    console.log("created tables")
     return db
 }
 
-const databaseTest = async () =>{
+
+async function printDatabaseStuff(){
+
     const db = await getDatabase("bluetoothDeviceReadings")
     const randomId = Math.random()
     await addDeviceToDatabase(db,randomId.toString(),"test")
 
-    const allRows = await db.getAllAsync<device>('SELECT * FROM devices')
-    for(const row of allRows){
-        console.log(row.macaddress, row.deviceName)
-    }
     let text=""
     for await (const row of db.getEachAsync<device>('SELECT * from devices')){
         console.log(row.macaddress, row.deviceName)
         text+=row.macaddress.toString() + "," + row.deviceName.toString() + "\n";
     }
-    let databaseView = <ThemedText>{text}</ThemedText>
+    return text
+}
+
+const databaseTest = ()  =>{
+    const [ deviceList, setDeviceList] = useState<string>();
+    printDatabaseStuff().then( (text) =>{
+        setDeviceList(text)
+    })
     return (
         <ThemedView>
             <ThemedText>Text should be outputted on debug console</ThemedText>
-            {databaseView}
+            <ThemedText>{ deviceList } </ThemedText>
         </ThemedView>
     )
 }
