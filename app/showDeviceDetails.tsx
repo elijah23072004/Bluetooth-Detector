74a01e9c66430542e7cc6f6a2b0cb3a5dce63511
Eli <eli.comp07@protonmail.com>
2026-04-24 16:27:59 +0100

    added all readings to be shown in showdevicedetails

--- app/showDeviceDetails.tsx
+++ app/showDeviceDetails.tsx
@@ -1,21 +1,23 @@
 import { useLocalSearchParams} from 'expo-router';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { getDeviceInfomation, getDatabase } from '@/utils/database';
+import { getDeviceInfomation, getDatabase, getDeviceReadingsString } from '@/utils/database';
 import { useState } from 'react';
 export default function showDeviceDetails(){
     const { macaddress} = useLocalSearchParams();
     const [text, setText] = useState("")
     if (text == ""){
         getDatabase().then( db => {
-            getDeviceInfomation(db,macaddress).then( info => {
+            getDeviceInfomation(db,macaddress).then( async info => {
+            let readingsString = await getDeviceReadingsString(macaddress,db)
             let out = "Device:"+info.device?.deviceName   
             out+="\n" 
             out+="macaddress:"+macaddress + "\n"
             out+="Most recent reading time:" + (new Date(info.mostRecentReadingTime)).toUTCString()+"\n"
             out+="Most recent deistance:"+ info.mostRecentReadingDistance.toString()+"\n"
             out+="First Reading Time:" + (new Date(info.firstReadingTime)).toUTCString() + "\n"
-            out+="Number of totatl Readings:" + info.numberOfDeviceReadings.toString()
+            out+="Number of totatl Readings:" + info.numberOfDeviceReadings.toString()+"\n"
+            out+="All readings:\n"+readingsString
             setText(out)
             db.closeSync()
             })
